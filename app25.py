1f8d2e5000e05c3cf1c31e000d1f07453964891b
ShivangGaur <38904285+ShivangGaur@users.noreply.github.com>
2024-03-26 22:40:35 +0530

    Update app25.py
    removed gradio

--- app25.py
+++ app25.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
@@ -9,7 +8,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-import gradio as gr
+from PyPDF2 import PdfReader
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
